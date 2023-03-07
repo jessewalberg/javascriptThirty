0f14de6daa4401c744164c25ae2231a1ca314c6b
jessewalberg <jesse@jessewalberg.com>
2023-03-07 16:11:03 -0500

    Finished project

--- js-and-clock/dist/index.js
+++ js-and-clock/dist/index.js
@@ -6,13 +6,18 @@ const makeClockAlive = () => {
     const date = new Date();
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
-    let hours = date.getMinutes();
+    let hours = (date.getHours() + 24) % 12 || 12;
+    console.log(hours, minutes, seconds);
     const secondsDegree = (seconds / 60) * 360;
-    console.log(secondHand);
     if (secondHand)
         secondHand.style.transform = `rotate(${secondsDegree}deg)`;
     const minutesDegree = (minutes / 60) * 360;
-    const hoursDegree = (minutes / 60) * 360;
+    console.log(minutesDegree);
+    if (minuteHand)
+        minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+    const hoursDegree = (hours / 12) * 360;
+    if (hourHand)
+        hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 };
 setInterval(() => {
     makeClockAlive();
--- js-and-clock/src/index.ts
+++ js-and-clock/src/index.ts
@@ -1,5 +1,5 @@
-const hourHand = document.querySelector(".hour-hand");
-const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand") as HTMLElement;
+const minuteHand = document.querySelector(".minute-hand") as HTMLElement;
 const secondHand = document.querySelector(".second-hand") as HTMLElement;
 
 const makeClockAlive = () => {
@@ -7,13 +7,15 @@ const makeClockAlive = () => {
 
   let seconds = date.getSeconds();
   let minutes = date.getMinutes();
-  let hours = date.getMinutes();
-
+  let hours = (date.getHours() + 24) % 12 || 12;
+  console.log(hours, minutes, seconds);
   const secondsDegree = (seconds / 60) * 360;
-  console.log(secondHand);
   if (secondHand) secondHand.style.transform = `rotate(${secondsDegree}deg)`;
   const minutesDegree = (minutes / 60) * 360;
-  const hoursDegree = (minutes / 60) * 360;
+  console.log(minutesDegree);
+  if (minuteHand) minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+  const hoursDegree = (hours / 12) * 360;
+  if (hourHand) hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 };
 
 setInterval(() => {
